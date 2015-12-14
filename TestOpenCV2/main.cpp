e0d97b5667952871733e773e363ec9cc2b4e3711
banbi <yr@banbidemacbook-pro.local>
2015-12-15 00:58:35 +0800

    Initial Commit

---
+++ TestOpenCV2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestOpenCV2
+//
+//  Created by banbi on 2015/12/15.
+//  Copyright © 2015年 banbi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
